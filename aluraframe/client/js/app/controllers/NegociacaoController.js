8b8ca88c84d139df11d06ad3e37678ecd5b4845b
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2021-02-01 20:59:58 -0300

    Isolando os helpers

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,28 +1,22 @@
 class NegociacaoController {
+  constructor() {
+    let $ = document.querySelector.bind(document);
 
-    constructor() {
-              let $ = document.querySelector.bind(document);
-  
-              this._inputData = $('#data');
-              this._inputQuantidade =  $('#quantidade');
-              this._inputValor = $('#valor');
-    }
-  
-    adiciona(event) {
-        event.preventDefault();
-    
-        let data = new Date(...
-            this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)
-        );
-        
-        let negociacao = new Negociacao(
-            data,
-            this._inputQuantidade.value,
-            this._inputValor.value
-          );
-        
-          console.log(negociacao);
-    }
+    this._inputData = $("#data");
+    this._inputQuantidade = $("#quantidade");
+    this._inputValor = $("#valor");
   }
+
+  adiciona(event) {
+    event.preventDefault();
+
+    let negociacao = new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
+      this._inputQuantidade.value,
+      this._inputValor.value
+    );
+
+    console.log(negociacao);
+    console.log(DateHelper.dataParaTexto(negociacao.data));
+  }
+}
